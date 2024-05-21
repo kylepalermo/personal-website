d9b3721570f00cdb38590a79d1cf3311a31d5d61
Kyle Palermo <kylepalermo@tamu.edu>
2024-05-21 01:38:16 -0500

    Prettier for navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,17 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname } from 'next/navigation';
+import { usePathname } from "next/navigation";
 import { ModeToggle } from "@/components/ModeToggle";
 
 const navbarItems = [
   {
-    name: 'Home',
-    href: '/',
+    name: "Home",
+    href: "/",
   },
   {
-    name: 'Projects',
-    href: '/projects',
+    name: "Projects",
+    href: "/projects",
   },
 ];
 
@@ -20,11 +20,19 @@ function Navbar() {
 
   return (
     <header className="flex w-full items-center justify-between pl-10 sticky top-0 backdrop-blur bg-background/50 z-10 border-b-[1px] border-border/40">
-      <nav className='flex w-full flex-row gap-4 items-center justify-center'>
+      <nav className="flex w-full flex-row gap-4 items-center justify-center">
         {navbarItems.map((item) => (
-          <Link className={'h-10 flex items-center justify-center duration-200 hover:text-foreground/70 navbar-item' + (pathname === item.href ? ' current' : '')} href={item.href} key={item.name}>
+          <Link
+            className={
+              "h-10 flex items-center justify-center duration-200 hover:text-foreground/70 navbar-item" +
+              (pathname === item.href ? " current" : "")
+            }
+            href={item.href}
+            key={item.name}
+          >
             {item.name}
-          </Link>))}
+          </Link>
+        ))}
       </nav>
       <ModeToggle />
     </header>
