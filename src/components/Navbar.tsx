c258877be5bfc3f28e4c8504dc6b43bde734812c
Kyle Palermo <kylepalermo@tamu.edu>
2024-05-21 01:32:08 -0500

    Improved navbar and navbar styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,40 +1,31 @@
 "use client";
 
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-  navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu";
 import Link from "next/link";
+import { usePathname } from 'next/navigation';
 import { ModeToggle } from "@/components/ModeToggle";
 
+const navbarItems = [
+  {
+    name: 'Home',
+    href: '/',
+  },
+  {
+    name: 'Projects',
+    href: '/projects',
+  },
+];
+
 function Navbar() {
+  const pathname = usePathname();
+
   return (
-    <header className="flex w-full items-center justify-between pl-10 sticky top-0 bg-background z-10 border-b-[1px] border-border">
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <Link href="/" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Home
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href="/projects" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Projects
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+    <header className="flex w-full items-center justify-between pl-10 sticky top-0 backdrop-blur bg-background/50 z-10 border-b-[1px] border-border/40">
+      <nav className='flex w-full flex-row gap-4 items-center justify-center'>
+        {navbarItems.map((item) => (
+          <Link className={'h-10 flex items-center justify-center duration-200 hover:text-foreground/70 navbar-item' + (pathname === item.href ? ' current' : '')} href={item.href} key={item.name}>
+            {item.name}
+          </Link>))}
+      </nav>
       <ModeToggle />
     </header>
   );
