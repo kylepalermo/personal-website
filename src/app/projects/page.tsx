e75ab920a047b51dd71a193e251b4d2f3f94cf7d
Kyle Palermo <kylepalermo@tamu.edu>
2024-05-22 00:54:55 -0500

    Added Connect Four project to project page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,16 +1,19 @@
 import ProjectCard from "@/components/ProjectCard";
 import nextImage from "../../../public/next.svg";
+import connectFourImage from "../../../public/connect-four.png";
 
 export default function Projects() {
   return (
     <main className="flex flex-col gap-4 p-4">
-      <h1 className="text-4xl font-bold w-fit relative before:content-[''] before:absolute before:w-full before:h-2 before:bottom-0 before:left-0 before:bg-primary/50 before:z-[-1]">Projects</h1>
+      <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold w-fit relative before:content-[''] before:absolute before:w-full before:h-2 before:bottom-0 before:left-0 before:bg-primary/50 before:z-[-1]">
+        Projects
+      </h1>
       <div className="flex flex-row gap-4 flex-wrap">
         <ProjectCard
-          title="Project 1"
-          description="Description 1"
-          image={nextImage}
-          link="https://www.google.com"
+          title="Connect Four"
+          description="A simple connect four game app built with JavaFX. Includes multiple levels of custom AI. Saves have been disabled for the online version."
+          image={connectFourImage}
+          link="http://connectfour.kylepalermo.net"
           color="primary"
         />
         <ProjectCard
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -6,14 +6,14 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
 
 interface ProjectCardProps {
   title: string;
   description: string;
-  image: string;
+  image: StaticImageData;
   link: string;
   color: string;
 }
@@ -30,15 +30,18 @@ function ProjectCard({
   switch (color) {
     case "primary":
       cardStyles = "border-l-[0.5rem] border-l-primary";
-      buttonStyles = "bg-primary text-primary-foreground hover:bg-primary relative after:rounded-[var(--radius)] after:shadow-[0_0_1rem_hsl(var(--primary))] after:opacity-0 after:transition-opacity after:duration-200 after:content-[''] after:absolute after:h-full after:w-full hover:after:opacity-100";
+      buttonStyles =
+        "bg-primary text-primary-foreground hover:bg-primary relative after:rounded-[var(--radius)] after:shadow-[0_0_1rem_hsl(var(--primary))] after:opacity-0 after:transition-opacity after:duration-200 after:content-[''] after:absolute after:h-full after:w-full hover:after:opacity-100";
       break;
     case "secondary":
       cardStyles = "border-l-[0.5rem] border-l-secondary";
-      buttonStyles = "bg-secondary text-secondary-foreground hover:bg-secondary relative after:rounded-[var(--radius)] after:shadow-[0_0_1rem_hsl(var(--secondary))] after:opacity-0 after:transition-opacity after:duration-200 after:content-[''] after:absolute after:h-full after:w-full hover:after:opacity-100";
+      buttonStyles =
+        "bg-secondary text-secondary-foreground hover:bg-secondary relative after:rounded-[var(--radius)] after:shadow-[0_0_1rem_hsl(var(--secondary))] after:opacity-0 after:transition-opacity after:duration-200 after:content-[''] after:absolute after:h-full after:w-full hover:after:opacity-100";
       break;
     case "accent":
       cardStyles = "border-l-[0.5rem] border-l-accent";
-      buttonStyles = "bg-accent text-accent-foreground hover:bg-accent relative after:rounded-[var(--radius)] after:shadow-[0_0_1rem_hsl(var(--accent))] after:opacity-0 after:transition-opacity after:duration-200 after:content-[''] after:absolute after:h-full after:w-full hover:after:opacity-100";
+      buttonStyles =
+        "bg-accent text-accent-foreground hover:bg-accent relative after:rounded-[var(--radius)] after:shadow-[0_0_1rem_hsl(var(--accent))] after:opacity-0 after:transition-opacity after:duration-200 after:content-[''] after:absolute after:h-full after:w-full hover:after:opacity-100";
       break;
   }
   return (
